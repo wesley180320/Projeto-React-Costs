2b9dd2b032e8160ede482db3cc35117b1047fd96
wesley180320 <estevaoowesley@gmail.com>
2022-06-28 21:25:45 -0400

    Conexão com a API

--- src/components/form/Select.jsx
+++ src/components/form/Select.jsx
@@ -7,7 +7,11 @@ const Select = ({text,name,options,handleOnChange, value}) =>{
 
         <div className={styles.form_control}>
             <label htmlFor={name}>{text}:</label>
-            <select name={name} id={name}> <option> Selecione uma opção </option> </select>
+            <select name={name} id={name}> <option> Selecione uma opção </option> 
+            {options.map((option) =>(
+                <option value={option.id} key={option.id}>{option.name}</option>
+            ))}
+            </select>
         </div>
 
     )
--- src/components/project/ProjectForm.jsx
+++ src/components/project/ProjectForm.jsx
@@ -1,11 +1,34 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import styles from "./ProjectForm.module.css"
 import Input from "../form/Input";
 import Select from "../form/Select";
 import SubmitButton from "../form/SubmitButton";
 
-const ProjectForm = ({btnText}) => {
+const ProjectForm = ({ btnText }) => {
 
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {// Buscar os elementos da api
+        fetch("http://localhost:5000/categories", {
+            method: "GET",
+            headers: {
+                'Content-Type': 'aplication/json',
+                  
+            },
+        })
+
+        // promisses
+        // Tranformar os dados em Json 
+.then((resp) => resp.json())
+        // Colocando os dados dentro da categoria   
+        .then((data) => {
+            setCategories(data)
+        })
+        .catch((err) => console.log(err))
+
+    }, [])
     return (
 
 
@@ -16,9 +39,9 @@ const ProjectForm = ({btnText}) => {
 
             <Input type="number" text="Orçamento do projeto" name="name" placeholder="Insira o orçamento do projeto" />
 
-            <Select name="category_id" text="Selecione a categoria"/>
+            <Select name="category_id" text="Selecione a categori" options={categories} />
 
-            <SubmitButton text={btnText}/>
+            <SubmitButton text={btnText} />
         </form>
 
 
